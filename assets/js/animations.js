75f96b6c7bb10b1bd6ab3e5f4e365289e18f0a06
samO <samolive@umich.edu>
2016-03-24 18:47:22 -0400

    commented

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,38 +1,55 @@
-$( document ).ready( function(){
+$( document ).ready(animateGraphic);
+$( window ).focus(function(){
+	clearAnimation();
+	animateGraphic();
+});
+
+
+function clearAnimation () {
+	cssAnimate($( "#jcol1" ), '', '', '', '');
+	cssAnimate($( "#jcol2" ), '', '', '', '');
+	cssAnimate($( "#jcol3" ), '', '', '', '');
+	cssAnimate($( "#jcol4" ), '', '', '', '');
+	cssAnimate($( "#jcol5" ), '', '', '', '');
+}
+
+function animateGraphic (){
 	var duration = "1s";
-	var delay = ".26s";
+	var timing = "linear";
+	var delay = ".251s";
 	var aniamtion = "tileAnimation";
 
 
 	$( "#jcol1" )[0].addEventListener("animationstart", function (){
-		cssAnimate($( "#jcol2" ), aniamtion, duration, delay);
+		cssAnimate($( "#jcol2" ), aniamtion, duration, timing, delay);
 	});
 
 	$( "#jcol2" )[0].addEventListener("animationstart", function (){
-		cssAnimate($( "#jcol3" ), aniamtion, duration, delay);
+		cssAnimate($( "#jcol3" ), aniamtion, duration, timing, delay);
 	});
 
 	$( "#jcol3" )[0].addEventListener("animationstart", function (){
-		cssAnimate($( "#jcol4" ), aniamtion, duration, delay);
+		cssAnimate($( "#jcol4" ), aniamtion, duration, timing, delay);
 	});
 
 	$( "#jcol4" )[0].addEventListener("animationstart", function (){
-		cssAnimate($( "#jcol5" ), aniamtion, duration, delay);
+		cssAnimate($( "#jcol5" ), aniamtion, duration, timing, delay);
 	});
 
 	$( "#jcol5" )[0].addEventListener("animationstart", function (){
-		cssAnimate($( "#jcol1" ), aniamtion, duration, delay);
+		cssAnimate($( "#jcol1" ), aniamtion, duration, timing, delay);
 	});
 
 	cssAnimate($( "#jcol1" ), aniamtion, duration, "0s"); 
+}
 
-});
+function cssAnimate (element, aniamtion, duration, timing, delay){
 
-function cssAnimate (element, aniamtion, duration, delay){
 	element.css("animation-duration", duration);
 	element.css("animation-delay", delay);
+	element.css("animation-timing-function", timing);
 	element.css("animation-name", aniamtion);
 	element[0].addEventListener("animationend", function(){
-		element.css("animation-name", '');
+		element.css("animation", '');
 	})
 }
